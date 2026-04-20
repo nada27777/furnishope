af9ef66374d37a810daa849d4a031042b437d7ff
nada27777 <nadaaboatla27@gmail.com>
2026-04-20 16:59:22 +0200

    Fix minor UI bugs and adjust slider logic

--- Furni.js
+++ Furni.js
@@ -43,8 +43,10 @@ document.getElementById('main-product-img').src = "picturesFurniture/sofa/" + im
 
 function openNav() {
     document.getElementById("side-menu").style.width = "250px";
+    document.getElementById("overlay").style.display = "block";
 }
 
 function closeNav() {
     document.getElementById("side-menu").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
 }
